923be503d8458eaac0e7fb66e2601e6022cf321d
Maxim Kalinovik <maxim.kalinovik@gmail.com>
2019-04-21 23:32:13 +0300

    T2: Create DbContect

---
+++ NugetAnalyzer.DAL/Context/NugetAnalyzerDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NugetAnalyzer.DAL.EntityConfigurations;
+using NugetAnalyzer.Domain;
+
+namespace NugetAnalyzer.DAL.Context
+{
+    public class NugetAnalyzerDbContext : DbContext
+    {
+        public NugetAnalyzerDbContext(DbContextOptions<NugetAnalyzerDbContext> options)
+            :base(options)
+        {
+        }
+
+        public DbSet<Package> Packages { get; set; }
+        public DbSet<ReferencePackage> ReferencePackages { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Repository> Repositories { get; set; }
+        public DbSet<Solution> Solutions { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new PackageConfiguration());
+            builder.ApplyConfiguration(new ReferencePackageConfiguration());
+            builder.ApplyConfiguration(new ProjectConfiguration());
+            builder.ApplyConfiguration(new RepositoryConfiguration());
+            builder.ApplyConfiguration(new SolutionConfiguration());
+            builder.ApplyConfiguration(new UserConfiguration());
+        }
+    }
+}
