cebff670f794593dd25f0331ee4a653317f00f64
maximKalinov <46676272+maximKalinov@users.noreply.github.com>
2019-04-22 11:41:37 +0300

    T1 (#8)
    * T1: Create application projects
    
    * T1: Create Test and Common projects
    
    * T1: Add NLog
    
    * T1: Add exception handling
    
    * T1: Fix references local system

---
+++ NugetAnalyzer/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NugetAnalyzer.Common.Middlewares;
+
+namespace NugetAnalyzer
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment environment)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(environment.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMiddleware<ExceptionHandlingMiddleware>();
+            app.UseMvc();
+        }
+    }
+}
