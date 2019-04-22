c70d8ea0eec961d4011d3f64217ad797e0eda456
Maxim Kalinovik <maxim.kalinovik@gmail.com>
2019-04-22 11:19:41 +0300

    T1: Add NLog

--- NugetAnalyzer/Program.cs
+++ NugetAnalyzer/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace NugetAnalyzer
 {
@@ -15,6 +17,12 @@ namespace NugetAnalyzer
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost
                 .CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
     }
 }
