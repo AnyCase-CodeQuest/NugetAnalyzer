14b64cb6bed57ac424159a09b75be919e9908ce6
Maxim Kalinovik <maxim.kalinovik@gmail.com>
2019-05-03 11:10:31 +0300

    21: Create NugetApiService

---
+++ NugetAnalyzer.BLL/Interfaces/INugetApiService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using NugetAnalyzer.Domain;
+
+namespace NugetAnalyzer.BLL.Interfaces
+{
+    public interface INugetApiService
+    {
+        Task<PackageVersion> GetLatestVersionPackageAsync(string packageName);
+        Task<DateTime?> GetPublishedDateByVersionAsync(string packageName, string version);
+    }
+}
---
+++ NugetAnalyzer.BLL/Services/NugetApiService.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NugetAnalyzer.BLL.Interfaces;
+using NugetAnalyzer.Domain;
+
+namespace NugetAnalyzer.BLL.Services
+{
+    public class NugetApiService : INugetApiService
+    {
+        private readonly IConfiguration configuration;
+
+        public NugetApiService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        private async Task<string> GetPackageMetadataAsync(string packageName, string version)
+        {
+            var url = $"{configuration["EndpointsNugetApi:PackageMetadata"]}/v3/registration3/{packageName.ToLowerInvariant()}/{version}.json";
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                return  await response.Content.ReadAsStringAsync();
+            }
+        }
+
+        private async Task<string> GetDataOfPackageVersionAsync(string query)
+        {
+            var url = $"{configuration["EndpointsNugetApi:Search"]}/query?q=PackageId:{WebUtility.UrlEncode(query ?? string.Empty)}&prerelease=false";
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+
+        private PackageVersion ParsePackageVersion(string response)
+        {
+            JObject jsonObject;
+            try
+            {
+                jsonObject = JObject.Parse(response);
+            }
+            catch (JsonReaderException)
+            {
+                return null;
+            }
+
+            var totalHitsString = (string)jsonObject["totalHits"];
+
+            if (!int.TryParse(totalHitsString, out var totalHits) || totalHits == 0)
+            {
+                return null;
+            }
+
+            var data = jsonObject["data"];
+            if (data == null)
+            {
+                return null;
+            }
+
+            var versionString = (string)data[0]["version"];
+            if (Version.TryParse(versionString, out var version))
+            {
+                return new PackageVersion
+                {
+                    Major = version.Major,
+                    Minor = version.Minor,
+                    Build = version.Build,
+                    Revision = version.Revision
+                };
+            }
+
+            return null;
+        }
+
+        private DateTime? ParsePublishedDate(string response)
+        {
+            JObject jsonObject;
+            try
+            {
+                jsonObject = JObject.Parse(response);
+            }
+            catch (JsonReaderException)
+            {
+                return null;
+            }
+
+            var publishedString = (string)jsonObject["published"];
+            if (DateTime.TryParseExact(
+                            publishedString,
+                            "MM/dd/yyyy HH:mm:ss",
+                            null,
+                            DateTimeStyles.None,
+                            out var published))
+            {
+                return published;
+            }
+
+            return null;
+        }
+
+        public async Task<PackageVersion> GetLatestVersionPackageAsync(string packageName)
+        {
+            if (packageName == null)
+            {
+                throw new ArgumentNullException(nameof(packageName));
+            }
+
+            var data = await GetDataOfPackageVersionAsync(packageName);
+
+            return ParsePackageVersion(data);
+        }
+
+        public async Task<DateTime?> GetPublishedDateByVersionAsync(string packageName, string version)
+        {
+            if (packageName == null)
+            {
+                throw new ArgumentNullException(nameof(packageName));
+            }
+
+            if (version == null)
+            {
+                throw new ArgumentNullException(nameof(packageName));
+            }
+
+            var data = await GetPackageMetadataAsync(packageName, version);
+
+            return ParsePublishedDate(data);
+        }
+    }
+}
