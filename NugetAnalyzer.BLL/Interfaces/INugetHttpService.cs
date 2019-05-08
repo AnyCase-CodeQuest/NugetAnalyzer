5359953d21bb1614cb8d8043f19a9cee6153c518
Maxim Kalinovik <maxim.kalinovik@gmail.com>
2019-05-08 16:23:06 +0300

    21: Refactoring

--- NugetAnalyzer.BLL/Interfaces/INugetHttpService.cs
+++ NugetAnalyzer.BLL/Interfaces/INugetHttpService.cs
@@ -1,7 +1,11 @@
-﻿namespace NugetAnalyzer.BLL.Interfaces
+﻿using System.Threading.Tasks;
+
+namespace NugetAnalyzer.BLL.Interfaces
 {
     public interface INugetHttpService
     {
-        
+        Task<string> GetPackageMetadataAsync(string packageName, string version);
+
+        Task<string> GetDataOfPackageVersionAsync(string query);
     }
 }
--- NugetAnalyzer.BLL/Services/NugetApiService.cs
+++ NugetAnalyzer.BLL/Services/NugetApiService.cs
@@ -14,35 +14,40 @@ namespace NugetAnalyzer.BLL.Services
     public class NugetApiService : INugetApiService
     {
         private const string publishedDateFormat = "MM/dd/yyyy HH:mm:ss";
-        private readonly IConfiguration configuration;
-        private readonly IHttpClientFactory clientFactory;
+        private readonly INugetHttpService httpService;
 
-        public NugetApiService(IConfiguration configuration, IHttpClientFactory clientFactory)
+        public NugetApiService(INugetHttpService httpService)
         {
-            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
-            this.clientFactory = clientFactory ?? throw new ArgumentNullException(nameof(clientFactory));
+            this.httpService = httpService ?? throw new ArgumentNullException(nameof(httpService));
         }
 
-        private async Task<string> GetPackageMetadataAsync(string packageName, string version)
+        public async Task<PackageVersion> GetLatestVersionPackageAsync(string packageName)
         {
-            var url = $"{configuration["EndpointsNugetApi:PackageMetadata"]}/v3/registration3/{packageName.ToLowerInvariant()}/{version}.json";
-            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            if (packageName == null)
+            {
+                throw new ArgumentNullException(nameof(packageName));
+            }
 
-            var client = clientFactory.CreateClient();
-            
-            var response = await client.SendAsync(request);
-            return  await response.Content.ReadAsStringAsync();
+            var data = await httpService.GetDataOfPackageVersionAsync(packageName);
+
+            return ParsePackageVersion(data);
         }
 
-        private async Task<string> GetDataOfPackageVersionAsync(string query)
+        public async Task<DateTime?> GetPublishedDateByVersionAsync(string packageName, string version)
         {
-            var url = $"{configuration["EndpointsNugetApi:Search"]}/query?q=PackageId:{WebUtility.UrlEncode(query ?? string.Empty)}&prerelease=false";
-            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            if (packageName == null)
+            {
+                throw new ArgumentNullException(nameof(packageName));
+            }
 
-            var client = clientFactory.CreateClient();
+            if (version == null)
+            {
+                throw new ArgumentNullException(nameof(packageName));
+            }
 
-            var response = await client.SendAsync(request);
-            return await response.Content.ReadAsStringAsync();
+            var data = await httpService.GetPackageMetadataAsync(packageName, version);
+
+            return ParsePublishedDate(data);
         }
 
         private PackageVersion ParsePackageVersion(string response)
@@ -106,35 +111,6 @@ namespace NugetAnalyzer.BLL.Services
             }
 
             return null;
-        }
-
-        public async Task<PackageVersion> GetLatestVersionPackageAsync(string packageName)
-        {
-            if (packageName == null)
-            {
-                throw new ArgumentNullException(nameof(packageName));
-            }
-
-            var data = await GetDataOfPackageVersionAsync(packageName);
-
-            return ParsePackageVersion(data);
-        }
-
-        public async Task<DateTime?> GetPublishedDateByVersionAsync(string packageName, string version)
-        {
-            if (packageName == null)
-            {
-                throw new ArgumentNullException(nameof(packageName));
-            }
-
-            if (version == null)
-            {
-                throw new ArgumentNullException(nameof(packageName));
-            }
-
-            var data = await GetPackageMetadataAsync(packageName, version);
-
-            return ParsePublishedDate(data);
         }
     }
 }
