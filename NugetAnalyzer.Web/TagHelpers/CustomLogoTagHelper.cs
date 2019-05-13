052d0442cd427e59539f014bd1a39f0ab1adc94f
Raman Zinevich <raman.zinevich.111@gmail.com>
2019-05-13 12:59:32 +0300

    T22: Custom logo tag helper was added.

---
+++ NugetAnalyzer.Web/TagHelpers/CustomLogoTagHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace NugetAnalyzer.Web.TagHelpers
+{
+    [HtmlTargetElement("custom-logo")]
+    public class CustomLogoTagHelper : TagHelper
+    {
+        public string Color { get; set; }
+        public string HoverColor { get; set; }
+        public string FontSize { get; set; }
+
+        public CustomLogoTagHelper()
+        {
+            Color = "rgba(0,0,0,.2)";
+            HoverColor = "rgba(0,0,0,.4)";
+            FontSize = "14";
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "span";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            var styles = $"color:{Color}; " +
+                         $"border: 2px solid {Color};" +
+                         $"font-size:{FontSize};" +
+                         "display:block;" +
+                         "border-radius:50%;" +
+                         "padding: 3px;";
+
+            output.Attributes.SetAttribute("style", styles);
+            output.Attributes.SetAttribute("onmouseover",
+                $"this.style.color=\"{HoverColor}\"; this.style.border=\"2px solid {HoverColor}\"");
+            output.Attributes.SetAttribute("onmouseout",
+                $"this.style.color=\"{Color}\"; this.style.border=\"2px solid {Color}\"");
+
+            output.Content.SetContent("NA");
+        }
+    }
+}
