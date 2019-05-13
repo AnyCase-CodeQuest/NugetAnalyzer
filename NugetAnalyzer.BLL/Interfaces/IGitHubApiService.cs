887014f54cf0fe7331ee90578ccf4356c37cf905
ArBond <artsemi.bandarenka@gmail.com>
2019-05-13 16:17:39 +0300

    initial commit

---
+++ NugetAnalyzer.BLL/Interfaces/IGitHubApiService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace NugetAnalyzer.BLL.Interfaces
+{
+    public interface IGitHubApiService
+    {
+        Task<IReadOnlyCollection<Repository>> GetUserRepositoriesAsync(string token);
+    }
+}
---
+++ NugetAnalyzer.BLL/Services/GitHubApiService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using NugetAnalyzer.BLL.Interfaces;
+using Octokit;
+using Octokit.Internal;
+
+namespace NugetAnalyzer.BLL.Services
+{
+    public class GitHubApiService : IGitHubApiService
+    {
+        public async Task<IReadOnlyCollection<Repository>> GetUserRepositoriesAsync(string token)
+        {
+            var github = new GitHubClient(new ProductHeaderValue("AspNetCoreGitHubAuth"),
+                new InMemoryCredentialStore(new Credentials(token)));
+            return await github.Repository.GetAllForCurrent();
+        }
+    }
+}
