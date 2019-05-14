8acb590c57c8a655ca6c0694e59f47e6f22fa600
Raman Zinevich <raman.zinevich.111@gmail.com>
2019-05-14 14:52:21 +0300

    T23: GetService was made.

---
+++ NugetAnalyzer.BLL/Services/GitService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using LibGit2Sharp;
+using NugetAnalyzer.BLL.Interfaces;
+
+namespace NugetAnalyzer.BLL.Services
+{
+    public class GitService : IGitService
+    {
+        public void CloneBranch(
+            string repositoryUrl,
+            string path,
+            string userToken,
+            string branchName = "master")
+        {
+            if (repositoryUrl == null)
+                throw new ArgumentNullException(nameof(repositoryUrl));
+
+            if (path == null)
+                throw new ArgumentNullException(nameof(path));
+
+            if (userToken == null)
+                throw new ArgumentNullException(nameof(userToken));
+
+            if (branchName == null)
+                throw new ArgumentNullException(nameof(branchName));
+
+            var cloneOptions = new CloneOptions
+            {
+                CredentialsProvider = (url, user, cred) =>
+                    new UsernamePasswordCredentials
+                    {
+                        Username = userToken,
+                        Password = string.Empty
+                    },
+                BranchName = branchName
+            };
+
+            Repository.Clone(repositoryUrl, path, cloneOptions);
+        }
+    }
+}
