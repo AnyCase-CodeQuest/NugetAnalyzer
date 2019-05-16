4a6671948a096906634b7f59d57aec081ee4b1e4
Raman Zinevich <raman.zinevich.111@gmail.com>
2019-05-16 13:58:05 +0300

    T39: Error controller was added.

---
+++ NugetAnalyzer.Web/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NugetAnalyzer.Web.Controllers
+{
+    [Route("error")]
+    public class ErrorController : Controller
+    {
+        [Route("404")]
+        public ActionResult NotFoundError()
+        {
+            return View();
+        }
+    }
+}
--- NugetAnalyzer.Web/Controllers/HomeController.cs
+++ NugetAnalyzer.Web/Controllers/HomeController.cs
@@ -8,5 +8,10 @@ namespace NugetAnalyzer.Web.Controllers
         {
             return View();
         }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
     }
 }
