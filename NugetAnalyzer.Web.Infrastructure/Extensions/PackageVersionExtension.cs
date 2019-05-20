c44654fcb1ea2a0ef4f23df909cfd4b101d438f4
Maxim Kalinovik <maxim.kalinovik@gmail.com>
2019-05-20 13:48:55 +0300

    16: Add package version extension

---
+++ NugetAnalyzer.Web.Infrastructure/Extensions/PackageVersionExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NugetAnalyzer.DTOs.Models;
+
+namespace NugetAnalyzer.Web.Infrastructure.Extensions
+{
+    public static class PackageVersionExtension
+    {
+        public static Version GetVersion(this PackageVersionDTO packageVersion)
+        {
+            if (packageVersion.Build < 0 && packageVersion.Revision < 0)
+            {
+                return new Version(packageVersion.Major, packageVersion.Minor);
+            }
+
+            return packageVersion.Revision < 0
+                ? new Version(packageVersion.Major, packageVersion.Minor, packageVersion.Build)
+                : new Version(packageVersion.Major, packageVersion.Minor, packageVersion.Build, packageVersion.Revision);
+        }
+    }
+}
