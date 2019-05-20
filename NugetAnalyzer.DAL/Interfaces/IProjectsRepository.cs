5dce877a5ff4094d4c957e01efb864384105e4d4
Maxim Kalinovik <maxim.kalinovik@gmail.com>
2019-05-20 13:47:52 +0300

    16: Add custom ProjectRepository

---
+++ NugetAnalyzer.DAL/Interfaces/IProjectsRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using NugetAnalyzer.Domain;
+
+namespace NugetAnalyzer.DAL.Interfaces
+{
+    public interface IProjectsRepository : IRepository<Project>
+    {
+        Task<IReadOnlyCollection<Project>> GetCollectionIncludePackage(Expression<Func<Project, bool>> predicate);
+    }
+}
---
+++ NugetAnalyzer.DAL/Repositories/ProjectsRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NugetAnalyzer.DAL.Context;
+using NugetAnalyzer.DAL.Interfaces;
+using NugetAnalyzer.Domain;
+
+namespace NugetAnalyzer.DAL.Repositories
+{
+    public class ProjectsRepository : Repository<Project>, IProjectsRepository
+    {
+        public ProjectsRepository(NugetAnalyzerDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IReadOnlyCollection<Project>> GetCollectionIncludePackage(Expression<Func<Project, bool>> predicate)
+        {
+            return await DbSet
+                .AsNoTracking()
+                .Include(project => project.ProjectPackageVersions)
+                .ThenInclude(projectPackageVersion => projectPackageVersion.PackageVersion)
+                .ThenInclude(packageVersion => packageVersion.Package)
+                .Where(predicate)
+                .ToListAsync();
+        }
+    }
+}
