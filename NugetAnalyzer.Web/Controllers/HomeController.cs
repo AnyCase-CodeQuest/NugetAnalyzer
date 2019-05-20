1c0fcafb82a6c6adfb2c3862c98f80d74cb25ba1
Maxim Kalinovik <maxim.kalinovik@gmail.com>
2019-05-20 16:30:04 +0300

    16: Add Report view

--- NugetAnalyzer.Web/Controllers/HomeController.cs
+++ NugetAnalyzer.Web/Controllers/HomeController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using NugetAnalyzer.BLL.Interfaces;
+using NugetAnalyzer.Common.Configurations;
 namespace NugetAnalyzer.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly PackageVersionConfigurations packageVersionConfigurations;
+
+        public HomeController(IOptions<PackageVersionConfigurations> packageVersionConfigurations)
+        {
+            this.packageVersionConfigurations = (packageVersionConfigurations ?? throw new ArgumentNullException(nameof(packageVersionConfigurations))).Value;
+        }
         public IActionResult Index()
         {
             return View();
@@ -17,6 +27,11 @@ namespace NugetAnalyzer.Web.Controllers
         public IActionResult About()
         {
             return View();
+        }
+
+        public IActionResult Rules()
+        {
+            return View(packageVersionConfigurations);
         }
     }
 }
