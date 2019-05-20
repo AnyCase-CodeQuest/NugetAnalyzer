5e62a3cfb2f61acefe525c47cb8b4ed877402136
Maxim Kalinovik <maxim.kalinovik@gmail.com>
2019-05-20 12:42:53 +0300

    16: Add models for report and converter

---
+++ NugetAnalyzer.Dtos/Converters/PackageVersionConverter.cs
@@ -0,0 +1,24 @@
+﻿using NugetAnalyzer.Domain;
+using NugetAnalyzer.DTOs.Models;
+
+namespace NugetAnalyzer.DTOs.Converters
+{
+    public static class PackageVersionConverter
+    {
+        public static PackageVersionDTO PackageVersionToPackageVersionDto(PackageVersion packageVersion)
+        {
+            return packageVersion == null
+                ? null
+                : new PackageVersionDTO
+                {
+                    Id = packageVersion.Id,
+                    Major = packageVersion.Major,
+                    Minor = packageVersion.Minor,
+                    Build = packageVersion.Build,
+                    Revision = packageVersion.Revision,
+                    PublishedDate = packageVersion.PublishedDate,
+                    PackageId = packageVersion.PackageId
+                };
+        }
+    }
+}
---
+++ NugetAnalyzer.Dtos/Models/PackageReportDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NugetAnalyzer.DTOs.Models.Reports;
+
+namespace NugetAnalyzer.DTOs.Models
+{
+    public class PackageReportDTO
+    {
+        public int PackageId { get; set; }
+
+        public DateTime? LastUpdateTime { get; set; } 
+
+        public string PackageName { get; set; }
+
+        public PackageVersionDTO Current { get; set; }
+
+        public PackageVersionDTO Latest { get; set; }
+
+        public PackageVersionComparisonReport Report { get; set; }
+    }
+}
