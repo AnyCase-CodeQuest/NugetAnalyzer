e177744a33b42bcb3ec09c9cbafd7d4b3164a4c4
maximKalinov <46676272+maximKalinov@users.noreply.github.com>
2019-08-02 10:13:23 +0300

    Create repository report page (#50) (#55)
    * 50: Fix layout, replace specefic class of bootstrap to custom class
    
    * 50 : Add partial view for reports
    
    * 50: Add Report action
    
    * 50: Fix name of variable to js
    
    * 50: Add attribute HttpGet and rename variable
    
    * 50: Refactoring

---
+++ NugetAnalyzer.Web/Controllers/RepositoryController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NugetAnalyzer.BLL.Interfaces;
+using NugetAnalyzer.DTOs.Models;
+using NugetAnalyzer.DTOs.Models.Reports;
+using NugetAnalyzer.Web.Infrastructure.HttpAccessors;
+
+namespace NugetAnalyzer.Web.Controllers
+{
+    public class RepositoryController : Controller
+    {
+        private readonly IRepositoryService repositoryService;
+        private readonly HttpContextInfoProvider httpContextInfoProvider;
+        private readonly ISourceService sourceService;
+        private readonly IProfileService profileService;
+
+        public RepositoryController(
+            IRepositoryService repositoryService,
+            HttpContextInfoProvider httpContextInfoProvider,
+            ISourceService sourceService,
+            IProfileService profileService)
+        {
+            this.repositoryService = repositoryService ?? throw new ArgumentNullException(nameof(repositoryService));
+            this.httpContextInfoProvider = httpContextInfoProvider ?? throw new ArgumentNullException(nameof(httpContextInfoProvider));
+            this.sourceService = sourceService ?? throw new ArgumentNullException(nameof(sourceService));
+            this.profileService = profileService ?? throw new ArgumentNullException(nameof(profileService));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Report()
+        {
+            string sourceName = httpContextInfoProvider.GetSourceName();
+            int externalId = httpContextInfoProvider.GetExternalId();
+            int sourceId = await sourceService.GetSourceIdByName(sourceName);
+
+            ProfileDTO profile =  await profileService.GetProfileBySourceIdAsync(sourceId, externalId);
+
+            ICollection<RepositoryReport> model = await repositoryService.GetAnalyzedRepositoriesAsync(p => p.UserId == profile.UserId);
+            
+            return View("_RepositoriesReport", model);
+        }
+    }
+}
---
+++ NugetAnalyzer.Web/wwwroot/scripts/Shared/accordion.js
@@ -0,0 +1,17 @@
+﻿(function accordion() {
+    let accordionElement = $(".div__accordion");
+	let i;
+
+    for (i = 0; i < accordionElement.length; i++) {
+		accordionElement[i].addEventListener("click", function () {
+			this.classList.toggle("div__active");
+			this.firstElementChild.classList.toggle("div__animation_active");
+			var panel = this.nextElementSibling;
+			if (panel.style.maxHeight) {
+				panel.style.maxHeight = null;
+			} else {
+				panel.style.maxHeight = panel.scrollHeight + "px";
+			}
+		});
+	}
+})();
